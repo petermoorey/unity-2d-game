5ab12c3e0b782896f6a1cd72a13e0ab42d54c37b
Peter Moorey <petermoorey@gmail.com>
2020-12-23 18:55:06 +0000

    Initial commit

---
+++ Assets/Platformer.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections; 
+using System.Collections.Generic; 
+using UnityEngine; 
+
+public class Platformer : MonoBehaviour 
+{
+    Rigidbody2D rb; 
+    public float speed;
+    public float jumpForce;
+    bool isGrounded = false; 
+    public Transform isGroundedChecker; 
+    public float checkGroundRadius; 
+    public LayerMask groundLayer;
+
+    // Start is called before the first frame update 
+    void Start() {
+        rb = GetComponent<Rigidbody2D>(); 
+    } 
+
+    // Update is called once per frame 
+    void Update() { 
+        Move();
+        Jump();
+        CheckIfGrounded();
+    }
+
+    void Move() { 
+        float x = Input.GetAxisRaw("Horizontal"); 
+        float moveBy = x * speed; 
+        rb.velocity = new Vector2(moveBy, rb.velocity.y); 
+    }
+
+    void Jump() { 
+    if (Input.GetKeyDown(KeyCode.Space) && isGrounded) { 
+        rb.velocity = new Vector2(rb.velocity.x, jumpForce); 
+        } 
+    }
+
+    void CheckIfGrounded() { 
+    Collider2D collider = Physics2D.OverlapCircle(isGroundedChecker.position, checkGroundRadius, groundLayer); 
+
+    if (collider != null) { 
+        isGrounded = true; 
+    } else { 
+        isGrounded = false; 
+        } 
+    }
+}
