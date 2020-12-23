99718f47ac111b585ce4ccd68661cbaea012d8ba
Peter Moorey <petermoorey@gmail.com>
2020-12-23 19:18:26 +0000

    Added basic camera tracking

--- Assets/Platformer.cs
+++ Assets/Platformer.cs
@@ -11,6 +11,8 @@ public class Platformer : MonoBehaviour
     public Transform isGroundedChecker; 
     public float checkGroundRadius; 
     public LayerMask groundLayer;
+    public float fallMultiplier = 2.5f; 
+    public float lowJumpMultiplier = 2f;
 
     // Start is called before the first frame update 
     void Start() {
@@ -21,6 +23,7 @@ public class Platformer : MonoBehaviour
     void Update() { 
         Move();
         Jump();
+        BetterJump();
         CheckIfGrounded();
     }
 
@@ -34,6 +37,14 @@ public class Platformer : MonoBehaviour
     if (Input.GetKeyDown(KeyCode.Space) && isGrounded) { 
         rb.velocity = new Vector2(rb.velocity.x, jumpForce); 
         } 
+    }
+
+    void BetterJump() {
+    if (rb.velocity.y < 0) {
+        rb.velocity += Vector2.up * Physics2D.gravity * (fallMultiplier - 1) * Time.deltaTime;
+    } else if (rb.velocity.y > 0 && !Input.GetKey(KeyCode.Space)) {
+        rb.velocity += Vector2.up * Physics2D.gravity * (lowJumpMultiplier - 1) * Time.deltaTime;
+        }   
     }
 
     void CheckIfGrounded() { 
---
+++ Assets/camera_tracking.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camera_tracking : MonoBehaviour
+{
+    [SerializeField]
+    protected Transform trackingTarget;
+    [SerializeField]
+    float xOffset;
+
+    [SerializeField]
+    float yOffset;
+
+    [SerializeField]
+    protected float followSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    float xTarget = trackingTarget.position.x + xOffset;
+    float yTarget = trackingTarget.position.y + yOffset;
+
+    float xNew = Mathf.Lerp(transform.position.x, xTarget, Time.deltaTime * followSpeed);
+    float yNew = Mathf.Lerp(transform.position.y, yTarget, Time.deltaTime * followSpeed);
+
+    transform.position = new Vector3(xNew, yNew, transform.position.z);
+    }
+}
